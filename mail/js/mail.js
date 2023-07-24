d2da9b43c1ce12b404bf2bd5943b95b58100a0df
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-07-24 14:55:27 +0200

    js: add button fuction

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -1,18 +1,35 @@
+//*Prendo input utente dal DOM//
+const inputUserEmail = document.getElementById('user-mail')
+
+//*Prendo il button dal DOM//
+const buttonSend = document.getElementById('send')
+
+//*Predno lo span dal DOM//
+const messageAccess = document.getElementById('message')
+
 //* Ho un Array che contiene una serie di email autorizzate ad accedere//
 const email = ['admin@gmail.com', 'user@gmail.com', 'boolean@gmail.com', 'employee@gmail.com'];
 
-// * Chiedo all'utente tramite prompt la sua email//
+buttonSend.addEventListener('click', function() {
+    
+    let mailFound = 'Spiacenti, Accesso negato. Riprova'
 
-const userEmail = prompt('Inserisci la tua email', 'admin@gmail.com');
+     const userEmail = inputUserEmail.value;
 
-let mailFound = 'Spiacenti, Accesso negato. Riprova'
-
-// * Creo ciclo for//
-for (let i = 0; i < email.length; i++){
-
-    //*Controllo tramite **IF** se l'email inserita dall'utente è uguale a quelle presenti nell'array.//
-    if( userEmail == email[i]) {
-        mailFound = 'Email trovata, Benvenuto'
+    // * Creo ciclo for//
+    for (let i = 0; i < email.length; i++){
+    
+        //*Controllo tramite **IF** se l'email inserita dall'utente è uguale a quelle presenti nell'array.//
+        if( userEmail == email[i]) {
+            mailFound = 'Email trovata, Benvenuto'
+        }
     }
-}
-console.log(mailFound);
+    messageAccess.innerText = mailFound
+})
+
+
+
+
+
+
+
