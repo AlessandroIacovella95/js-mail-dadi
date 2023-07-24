053db20421eafa00c1b3cbd9734e2912e0ceb100
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-07-24 16:21:46 +0200

    js: button fuction

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,18 +1,46 @@
+//*const per generare un numero da 1 a 6//
+
 const max = 6;
 const min = 1;
 
-const randomCpu = Math.floor (Math.random() * max)+ min;
 
-console.log("Numero Computer: " + randomCpu);
+//*Prendo il div user_number dal DOM//
 
-const randomUser = Math.floor (Math.random() * max)+ min;
+const userNumber = document.getElementById('user_number');
 
-console.log("Numero Utente: " + randomUser);
+//*Prendo il div cpu_number dal DOM//
 
-if (randomCpu > randomUser ){
-    console.log('il computer ha vinto')
-} else if (randomUser > randomCpu ){
-    console.log('utente ha vinto');
-} else{
-    console.log('i numeri sono uguali, pareggio')
-}
+const cpuNumber = document.getElementById('cpu_number');
+
+//*Prendo il div result dal DOM//
+
+const resultFinal = document.getElementById('result');
+
+//*Prendo il button dal DOM//
+const button = document.getElementById ('button_generate');
+
+
+//* Button Function//
+button.addEventListener('click', function() {
+    
+    //*Genero in maniera randomica due numeri, uno per il computer e uno per l'utente//
+
+    const randomCpu = Math.floor (Math.random() * max)+ min;
+    
+    const randomUser = Math.floor (Math.random() * max)+ min;
+
+    userNumber.innerHTML = "Numero Utente: " + randomUser
+
+    cpuNumber.innerHTML = "Numero Computer: " + randomCpu
+
+    //*Creo la condizione//
+
+    if (randomCpu > randomUser ){
+    resultFinal.innerText = 'Il computer ha vinto'
+    } else if (randomUser > randomCpu ){
+        resultFinal.innerText = "l'Utente ha vinto"
+    } else{
+        resultFinal.innerText= 'I numeri sono uguali, pareggio'
+    }
+
+})
